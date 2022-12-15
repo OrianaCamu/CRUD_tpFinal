a656e0d27d03d6ddf3d8767cab1f7ff7b1eb7ad2
oriana camussoni <camussonioriana@gmail.com>
2022-12-15 12:39:24 -0300

    agregado de boton

--- crear_medicos.php
+++ crear_medicos.php
@@ -7,24 +7,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-     <div class="container">
-         <div class="row">
+    <div class="container">
+        <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <h4>
-                            Registrar medico
-                            <a href="" class="btn btn-danger float-end"> Regresar</a>
-                    
-                        </h4>
-                    </div>
+                  <div class="card-header">
+                    <h4>
+                    Registrar medico
+                    <a href="index.php" class="btn btn-danger float-end">Regresar</a>
+                    </h4>
+                  </div>
+
                 </div>
-
-            </div> 
-         </div>
-     </div>
-
-
+            </div>
+        </div>
+    </div>
+    
 
 
 
