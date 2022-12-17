de5aedd9f3c86b597afb25fceea51bedcb0758c8
oriana camussoni <camussonioriana@gmail.com>
2022-12-17 10:32:21 -0300

    nuevo codigo

--- index.php
+++ index.php
@@ -12,12 +12,9 @@ require "conexion.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CRUD PHP MYSQL</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-
-
   </head>
-
-<div class="container">
+  <body>
+     <div class="container">
           <?php
           if (isset($_SESSION['mensaje'])){
               if (!$_SESSION['error']){
@@ -34,8 +31,8 @@ require "conexion.php";
               <?php
               }
               unset($_SESSION['mensaje']);
-              unset($_SESSION['error']);
           }
+          
           ?>
 
 
@@ -46,52 +43,74 @@ require "conexion.php";
            <div class="card">
             <div class="card-header">
                 <h4>
-                    Lista de Profesionales
-                    <a href="crear_medicos.php" class="btn btn-success float-end">Agregar nuevo</a>
+                    Registrar medico
+                    <a href="index.php" class="btn btn-danger float-end">Regresar</a>
                 </h4>
             </div>
                <div class="card-body">
-                    <table class="table table-bordered table-striped">
-                      <thead>
-                        <tr>
-                           <th>ID</th>
-                           <th>nombre</th>
-                           <th>apellido</th>
-                           <th>edad</th>
-                           <th>especialidad</th>
-                           <th>pais</th>
-                           <th>acciones</th>
-                        </tr>
-                        <?php
-                               $res= $conexion->query("SELECT M.*, P.nombre as pais FROM `medicos` M INNER JOIN paises P ON P.id=M.id_pais;");
+                <form action ="guardar.php" method ="post">
+                  <div class="mb-3">
+                        <label for="">Nombre medico</label>
+                        <input type = "text" name= "nombre" class= "form-control">
+                  
+                  </div>
+
+                  <div class="mb-3">
+                        <label for="">Apellido medico</label>
+                        <input type = "text" name= "apellido" class= "form-control">
+                  
+                  </div>
+
+                  <div class="mb-3">
+                        <label for="">Edad medico</label>
+                        <input type = "number" name= "edad" class= "form-control">
+                  
+                  </div>
+
+                  <div class="mb-3">
+                       <label for="">Especialidad medico</label>
+                       <input type = "text" name= "especialidad" class= "form-control">
+                  
+                  </div>
+
+                  <div class="mb-3">
+                       <label for="">Pais</label>
+                          <select name="pais" id=" " class="form-select">
+                            <option value="">Seleccione un pais </option>
+                            <?php
+                               $res= $conexion->query("SELECT * FROM `paises` order by nombre");
                                while ($fila = $res->fetch_object()){
                                 ?>
-                                 <tr>
-                          <td><?php echo $fila->id; ?></td>
-                          <td><?php echo $fila->nombre; ?></td>
-                          <td><?php echo $fila->apellido; ?></td>
-                          <td><?php echo $fila->edad; ?></td>
-                          <td><?php echo $fila->especialidad; ?></td>
-                          <td><?php echo $fila->pais; ?></td>
-                          <td>
-                            <a href="editar_medicos.php?id=<?php echo md5($fila->id); ?>" class="btn btn-primary">
-                              Editar
-                               </a>
-                      
-                          </td>
-                        </tr>
-                              
+                                <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre; ?> </option>
                             <?php
 
                                }
 
                             ?>
-                       
-                      </thead>
-                    </table>
+
+
+                          </select>
+                  </div>
+
+                  <div class="mb-3">
+                     <button type= "submit" name="btnGuardar" class="btn btn-primary"> Guardar medico </button>
+                  
+                  </div>
+
+
+
+
+
+
+
+                </form>
                </div>
            </div>
         </div>
        </div>
      </div>
 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
